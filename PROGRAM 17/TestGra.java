5d3125ef659784f88790fc5a7eab05b0a07e5f8d
Sivanis29 <sivanis851@gmail.com>
2026-03-16 11:22:49 +0530

    update

---
+++ PROGRAM 17/TestGra.java
@@ -0,0 +1,11 @@
+import Graphics.area;
+public class TestGra{
+public static void main (String[]args){
+area g =new area();
+System.out.println("araa of rectange:"+g.rect(4,5));
+System.out.println("araa of triangle:"+g.tria(3,4.5));
+System.out.println("araa of Square:"+g.squ(5));
+System.out.println("araa of circle:"+g.cir(4.5));
+}
+}
+
